5fc502143f6e656c3874c2b3a48d18ccfd86ebe1
fredieposh <maor1234@gmail.com>
2025-11-03 22:36:08 +0200

    Import sections containers to App module

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LeftSide from './components/LeftSide.jsx'
+import RightSide from './components/RightSide.jsx'
+
 
 function App() {
-
+  return (
+    <>
+      <LeftSide />
+      <RightSide />
+    </>
+  )
 }
 
 export default App
