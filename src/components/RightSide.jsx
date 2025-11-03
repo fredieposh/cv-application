1acebcf6cadf487b36c87577e93d4bbcb8a0b80a
fredieposh <maor1234@gmail.com>
2025-11-03 22:35:19 +0200

    Create and style the container for the output sections

---
+++ src/components/RightSide.jsx
@@ -0,0 +1,24 @@
+import '../styles/RightSide.css'
+
+export default function RightSide() {
+    const sectionsNames = [
+        {
+            id: 0,
+            name: 'general-content',
+        },
+        {
+            id: 1,
+            name: 'educational-content',
+        },
+        {
+            id: 2,
+            name: 'experience-content',
+        },
+    ];
+
+    return (
+        <div className='right-side'>
+
+        </div>
+    )
+}
