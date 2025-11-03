56a83bcc1a817a2e0fa3796771f6d7b26e222d45
fredieposh <maor1234@gmail.com>
2025-11-03 22:34:03 +0200

    Create and style the container for the input sections

---
+++ src/components/LeftSide.jsx
@@ -0,0 +1,24 @@
+import '../styles/LeftSide.css'
+
+export default function LeftSide() {
+    const sectionsNames = [
+        {
+            id: 0, 
+            name: 'general',
+        },
+        {
+            id: 1, 
+            name: 'educational',
+        },
+        {
+            id: 2, 
+            name: 'experience'
+        },
+    ];
+
+    return (
+        <div className="left-side">
+
+    </div>
+    )
+}
