3f350e9ea62812c73cd87e57aeade85c9bab68d7
fredieposh <maor1234@gmail.com>
2025-11-04 21:50:14 +0200

    Create the right side

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,24 +1,36 @@
+import { useState } from 'react'
 import '../styles/RightSide.css'
+import sections from '../section'
 
 export default function RightSide() {
-    const sectionsNames = [
-        {
-            id: 0,
-            name: 'general-content',
-        },
-        {
-            id: 1,
-            name: 'educational-content',
-        },
-        {
-            id: 2,
-            name: 'experience-content',
-        },
-    ];
+    const [divList, setDivList] = useState(sections);
+    const root = divList[0];
+    const containersIdList = root.childIds;
 
     return (
-        <div className='right-side'>
-
+        <div className = 'right-side'>
+            <h2>Your CV</h2>
+            {containersIdList.map(id => (
+                <PlaceTree
+                key={id}
+                id={id}
+                elementById={divList} />
+            ))}
         </div>
     )
+};
+
+function PlaceTree({ id, elementById}) {
+    const containerName = elementById[id].name
+    const childIds = elementById[id].childIds
+    return (
+            <div className = {containerName}>
+                {childIds.map(childId => (
+                    <div key={childId} className={elementById[childId].name}>
+                        <p>{elementById[childId].name.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join(' ') + ': '}</p>
+                        <p></p>
+                    </div>
+                ))}
+            </div>
+        )
 }
